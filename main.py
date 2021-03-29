4f71222fee07810dc053f430c7c7c640e09724e1
Fer <marinbcn169@gmail.com>
2021-03-29 19:24:59 +0200

    wip

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import re
+import random
+
 def get_number_participants():
     n_participants = -1
     while True:
@@ -11,6 +14,21 @@ def get_number_participants():
             print("That was no valid number. Try again...")
     return n_participants
 
+def creat_structure_info(participant_info):
+    try:
+        if(len(participant_info) == 2):
+            participant = [participant_info[0], participant_info[1], -1]
+            return participant
+    except ValueError:
+        print("Invalid participant info. Try again...")
+
+def regex_user(participant_info):
+    # cheeck if correct name and email wit re
+    if (bool(re.fullmatch('[A-Za-z]{2,25}( [A-Za-z]{2,25})?', participant_info[0])) and (bool(re.fullmatch('^[a-z0-9]+[\._]?[ a-z0-9]+[@]\w+[. ]\w{2,3}$', participant_info[1])))):
+        return True
+    else:
+        return False
+
 def get_participants():
     participants_list = []
     n_participants = get_number_participants()
@@ -20,34 +38,40 @@ def get_participants():
         if counter == n_participants:
             break
         try:
-            participant = input("Please enter a participant, name and email sepparated by comas\nExample: name,name@email.com:\n")
-            #verify correct imput
-            participants_list.append(creat_tuple_info(participant))
-            counter +=1
+            participant_info = []
+            participant_name = input("Please enter the name of a participant: ")
+            participant_email = input("Please enter the email of a participant: ")
+            participant_info.append(participant_name)
+            participant_info.append(participant_email)
+
+            if(regex_user(participant_info)):
+                participants_list.append(creat_structure_info(participant_info))
+                counter +=1
+            else:
+                print("Invalid user...")
         except ValueError:
-            print("That was no valid number. Try again...")
+            print("Invalid user...")
 
-    print(participants_list)
+    return participants_list
 
-def creat_tuple_info(participant_info):
-    try:
-        x = participant_info.split(", ")
-        print(x)
-        participant = ("","")
-
-        if(len(x) == 2):
-            participant = (x[0], x[1])
-            # TODO: check regular expresions name and email
-            return participant
-    except ValueError:
-        print("Invalid participant info. Try again...")
-
-def generate_friend():
+def generate_friend(participants_list):
     # we make the random friend per user
     print("Getting participnts...")
+    randomized_list = participants_list[:]
+    while True:
+        random.shuffle(randomized_list)
+        for a, b in zip(participants_list, randomized_list):
+            if a == b:
+                break
+        else:
+            return randomized_list
+
 def send_mails():
     print("sending mails...")
 
 if __name__ == "__main__":
-    get_participants()
-    get_participants()
+    participants = get_participants()
+    friends = generate_friend(participants)
+    #TODO asignar a que usuario le toca cada uno
+    print(participants)
+    print(friends)
